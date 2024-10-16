5ccdf630d9a57a5af426148469645bf4169f0586
Jayanth555 <62601803+Jayanth555@users.noreply.github.com>
2024-10-16 11:11:28 -0400

    Update main.tf

--- ec2-module/main.tf
+++ ec2-module/main.tf
@@ -1,9 +1,8 @@
-
 resource "aws_instance" "ec2_instance" {
-    ami = "${var.ami_id}"
-    instance_type = "t2.micro"
-    subnet_id = "subnet-0c83f314e971584da" 
-  tags = {
-    Name = "MyEC2Instance"
-  }
-} 
+    ami           = "${var.ami_id}"
+    instance_type = var.instance_type
+    subnet_id     = var.subnet_id
+    tags = {
+        Name = var.instance_name
+    }
+}
