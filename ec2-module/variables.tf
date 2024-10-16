35b7bb2f606c81c16b339b8c6ddc198dc03be932
Jayanth555 <62601803+Jayanth555@users.noreply.github.com>
2024-10-16 11:11:56 -0400

    Update variables.tf

--- ec2-module/variables.tf
+++ ec2-module/variables.tf
@@ -1,25 +1,22 @@
-
 variable "instance_name" {
-        description = "Name of the instance to be created"
-        default = "awsbuilder-demo"
+    description = "Name of the instance to be created"
+    default     = "awsbuilder-demo"
 }
 
 variable "instance_type" {
-        default = "t2.micro"
+    default = "t2.micro"
 }
 
-
 variable "ami_id" {
-        description = "The AMI to use"
-        default = "ami-09d56f8956ab235b3"
+    description = "The AMI to use"
+    default     = "ami-09d56f8956ab235b3"
 }
 
 variable "number_of_instances" {
-        description = "number of instances to be created"
-        default = 1
+    description = "number of instances to be created"
+    default     = 1
 }
 
 variable "subnet_id" {
-   description = "The subnet id"
+    description = "The subnet id"
 }
-
